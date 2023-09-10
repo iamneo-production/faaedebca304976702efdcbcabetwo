7d7152e5e3e700f3cf36bea564152c8af13999bc
Iamneo <techadmin@iamneo.ai>
2023-09-10 16:59:53 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
-// Initial game state
 let cells = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let result = document.querySelector('.result');
 let btns = document.querySelectorAll('.btn');
-let conditions = [
+const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -14,51 +13,50 @@ let conditions = [
     [2, 4, 6]
 ];
 
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
+function checkWin() {
+    for (let condition of winConditions) {
+        const [a, b, c] = condition;
+        if (cells[a] && cells[a] === cells[b] && cells[b] === cells[c]) {
+            return cells[a];
+        }
+    }
+    if (!cells.includes('')) {
+        return 'draw';
+    }
+    return null;
+}
 
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
+function handleMove(element, index) {
+    if (cells[index] || checkWin()) {
+        return;
+    }
+    cells[index] = currentPlayer;
+    element.value = currentPlayer;
+    element.disabled = true;
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    const winner = checkWin();
+    if (winner === 'draw') {
+        result.innerHTML = 'It\'s a draw!';
+    } else if (winner) {
+        result.innerHTML = `Player ${winner} Won 🎉`;
+        btns.forEach((btn) => btn.disabled = true);
+    } else {
+        result.innerHTML = `Player ${currentPlayer} Turn`;
+    }
+}
 
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
-
-    // Your code to update the game state and check for a win
-    // ...
-
-    // Your code to display the current player's turn
-    // ...
-
-    // Your code to handle button and cell interactions
-    // ...
-};
-
-    /*
-    **Part 2: Reset Function (Add your code here)**
-
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
-});
+function resetGame() {
+    cells = ['', '', '', '', '', '', '', '', ''];
+    btns.forEach((btn) => {
+        btn.value = '';
+        btn.disabled = false;
+    });
+    currentPlayer = 'X';
+    result.innerHTML = 'Player X Turn';
+}
 
 document.querySelector('#reset').addEventListener('click', resetGame);
+
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => handleMove(btn, i));
+});
